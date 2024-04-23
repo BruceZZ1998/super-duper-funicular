b8155ba635182767a3bbf60019483af58274d15f
BruceZZ1998 <zz980317@gmail.com>
2024-04-23 07:51:43 -0700

    try 3

--- src/main/java/com/bfs/springdatademo/config/SwaggerConfig.java
+++ src/main/java/com/bfs/springdatademo/config/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    //hello
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -18,5 +19,7 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+
+
     }
 }
